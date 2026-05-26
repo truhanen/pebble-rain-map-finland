3e17b81648f080123503c16e2ee0b6aa8981cd51
Tuukka Ruhanen <tuukka.t.ruhanen@gmail.com>
2026-05-26 10:50:57 +0300

    CHORE: Add legend window to generated screenshots

--- scripts/create_screenshots.sh
+++ scripts/create_screenshots.sh
@@ -3,13 +3,13 @@ set -euo pipefail
 
 EMULATOR="${PEBBLE_EMULATOR:-emery}"
 SS_DIR="screenshots"
-PNG_DIR="$SS_DIR/png"
-mkdir -p "$PNG_DIR"
+SEQUENCE_PNG_DIR="$SS_DIR/sequence_png"
+mkdir -p "$SEQUENCE_PNG_DIR"
 
 ss_index=0
 ss() {
     local filename
-    filename="$(printf '%s/%02d_%s.png' "$PNG_DIR" "$ss_index" "$1")"
+    filename="$(printf '%s/%02d_%s.png' "$SEQUENCE_PNG_DIR" "$ss_index" "$1")"
     pebble screenshot --emulator "$EMULATOR" --no-open "$filename"
     ss_index=$((ss_index + 1))
 }
@@ -35,6 +35,8 @@ btn up;     ss "up_6";
 ss "before_select"
 btn select; ss "select_1"
 btn select; ss "select_2"
+btn select; ss "legend"
+btn back
 ss "before_down"
 btn down;   ss "down_1"
 btn down;   ss "down_2"
@@ -47,13 +49,14 @@ btn back; ss "back_1"
 btn back; ss "back_2"
 ss "end"
 
-echo "Done. Screenshots saved to $PNG_DIR/"
+echo "Done. Screenshots saved to $SEQUENCE_PNG_DIR/"
 
-echo "Copying selected screenshots..."
-cp "$PNG_DIR/00_start.png"    "$SS_DIR/screenshot_02.png"
-cp "$PNG_DIR/08_select_1.png" "$SS_DIR/screenshot_03.png"
-cp "$PNG_DIR/16_down_6.png"   "$SS_DIR/screenshot_04.png"
+echo "Copying/moving selected screenshots..."
+mv "$SEQUENCE_PNG_DIR/10_legend.png"   "$SS_DIR/screenshot_02.png"
+cp "$SEQUENCE_PNG_DIR/00_start.png"    "$SS_DIR/screenshot_03.png"
+cp "$SEQUENCE_PNG_DIR/08_select_1.png" "$SS_DIR/screenshot_04.png"
+cp "$SEQUENCE_PNG_DIR/17_down_6.png"   "$SS_DIR/screenshot_05.png"
 
 echo "Creating GIF..."
-magick -delay 50 -loop 0 ${PNG_DIR}/*.png -layers Optimize "$SS_DIR/screenshot_01.gif"
+magick -delay 50 -loop 0 ${SEQUENCE_PNG_DIR}/*.png -layers Optimize "$SS_DIR/screenshot_01.gif"
 echo "GIF saved to $SS_DIR/screenshot_01.gif"
